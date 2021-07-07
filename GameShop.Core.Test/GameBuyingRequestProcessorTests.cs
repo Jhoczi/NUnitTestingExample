716ac88f2dee9d0084edcc8c85ce2b0cc1042084
Jakub Hoczek <hoczek.jakub@gmail.com>
2021-07-07 09:54:36 +0200

    'Init'

---
+++ GameShop.Core.Test/GameBuyingRequestProcessorTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GameShop.Core.Test
+{
+    [TestFixture]
+    public class GameBuyingRequestProcessorTests
+    {
+        private GameBuyingRequestProcessor _processor;
+
+        public GameBuyingRequestProcessorTests()
+        {
+            _processor = new GameBuyingRequestProcessor();
+        }
+        [Test]
+        public void ShouldReturnGameBuyingResultWithRequestValues()
+        {
+            
+            // Arrange
+            var request = new GameBuyingRequest()
+            {
+                FirstName = "Cezary",
+                LastName = "Walenciuk",
+                Email = "walenciukc@gmail.com",
+                Date = DateTime.Now
+            };
+            
+            // Act
+            GameBuyingResult result = _processor.BuyGame(request);
+            
+            // Assert
+            Assert.NotNull(result);
+            Assert.AreEqual(request.FirstName, result.FirstName);
+            Assert.AreEqual(request.LastName, result.LastName);
+            Assert.AreEqual(request.Email, result.Email);
+            Assert.AreEqual(request.Date, result.Date);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () =>  _processor.BuyGame(null)
+                );
+
+            Assert.AreEqual("request",exception.ParamName);
+        }
+
+        [Test]
+        public void ShouldReturnStatusTrueWhenSendedCorrectValues()
+        {
+            var request = new GameBuyingRequest()
+            {
+                FirstName = "Cezary",
+                LastName = "Walenciuk",
+                Email = "walenciukc@gmail.com",
+                Date = DateTime.Now
+            };
+
+            GameBuyingResult result = _processor.BuyGame(request);
+            
+            Assert.AreEqual(true,result.IsStatusOk);
+            Assert.AreEqual(0, result.Errors.Count);
+        }
+        
+    }
+    
+}
---
+++ GameShop.Core/GameBuyingResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameShop.Core
+{
+    public class GameBuyingResult
+    {
+        public GameBuyingResult()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsStatusOk { get; set; }
+        public List<string> Errors { get; set; }
+        
+    }
+
+    public class GameBuyingRequestProcessor
+    {
+        public GameBuyingResult BuyGame(GameBuyingRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException(nameof(request));
+            
+            var result = new GameBuyingResult();
+
+            result.FirstName = request.FirstName;
+            result.LastName = request.LastName;
+            result.Date = request.Date;
+            result.Email = request.Email;
+            result.IsStatusOk = true;
+            result.Errors = new List<string>();
+
+            return result;
+        }
+    }
+    
+}
+
